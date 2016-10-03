2cc179c435aeeebdc2b4f441a81a233d3c577942
aprilnovak <novak@berkeley.edu>
2016-10-03 14:05:51 -0700

    Updated quadrature.

--- HW2/quadrature.m
+++ HW2/quadrature.m
@@ -7,6 +7,9 @@ switch shape_order
     case 3
         wt = [5/9, 8/9, 5/9];
         qp = [-sqrt(3/5), 0, sqrt(3/5)];
+    case 4
+        wt = [(322-13*sqrt(70))/900, (322+13*sqrt(70))/900, 128/225, (322+13*sqrt(70))/900, (322-13*sqrt(70))/900];
+        qp = [-(1/3)*sqrt(5+2*sqrt(10/7)), -(1/3)*sqrt(5-2*sqrt(10/7)), 0.0, (1/3)*sqrt(5-2*sqrt(10/7)), (1/3)*sqrt(5+2*sqrt(10/7))];
     otherwise
         disp('You entered an unsupported shape function order for the quadrature rule.');
 end
