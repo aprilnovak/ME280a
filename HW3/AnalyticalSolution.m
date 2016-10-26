6bda1286883c9284f0bdab57f00c2b516c7f26fb
aprilnovak <novak@berkeley.edu>
2016-10-26 08:58:05 -0700

    Consolidated analytical solution into one function.

--- HW3/AnalyticalSolution.m
+++ HW3/AnalyticalSolution.m
@@ -38,5 +38,13 @@ solution_analytical_derivative = (k_freq^3) * (gamma .* physical_domain .* sin(g
 
 
 
+function [RHS_value] = RHSFunction(gamma, k_freq, space_blocks, E_blocks, node_number)
+% function to repeatedly evaluate \circled{A} in the document
+
+loc = space_blocks(node_number);
+RHS_value = - loc * (k_freq^3) * cos(gamma * loc) / (gamma ^2) + 2 * k_freq ^ 3 * sin(gamma * loc) / (gamma ^ 3);
+end
+
+
 end
 
