5e2d42389693e4958a6c4e1e5e0f3e22e00d11b0
aprilnovak <novak@berkeley.edu>
2016-10-28 10:26:19 -0700

    Skeleton of HW 4 - copied over HW 1.

---
+++ HW4/BCnodes.m
@@ -0,0 +1,40 @@
+% Script to return the node numbers associated with different types of
+% boundary conditions
+
+function [dirichlet_nodes, neumann_nodes, a_k] = BCnodes(left, right, left_value, right_value, num_nodes)
+
+% arrays that hold the nodes in the first row and the values in each column
+dirichlet_nodes = [];
+neumann_nodes = [];
+
+% assign the nodes to either dirichlet or neumann BCs
+i = 1;
+switch left
+    case 'Dirichlet'
+        dirichlet_nodes(1, i) = 1;
+        dirichlet_nodes(2, i) = left_value;
+    case 'Neumann'
+        neumann_nodes(1, i) = 1;
+    otherwise
+        disp('You entered an incorrect field for the type of BC on the left boundary.');
+end
+
+i = i + 1;
+switch right
+    case 'Dirichlet'
+        dirichlet_nodes(1, i) = num_nodes;
+        dirichlet_nodes(2, i) = right_value;
+    case 'Neumann'
+        neumann_nodes(1, i) = num_nodes;
+    otherwise
+        disp('You entered an incorrect field for the type of BC on the right boundary.');
+end
+
+a_k = [];
+
+if isempty(dirichlet_nodes)
+    disp('no dirichlet nodes')
+else
+    a_k = dirichlet_nodes(2,:)';
+end
+
