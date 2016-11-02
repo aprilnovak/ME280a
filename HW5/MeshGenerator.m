258b55d2155c2f2352f3ee5c03bb2a70f70588d7
aprilnovak <novak@berkeley.edu>
2016-11-02 16:40:49 -0700

    Meshed in one single plane.

--- HW5/MeshGenerator.m
+++ HW5/MeshGenerator.m
@@ -1,12 +1,17 @@
 % Mesh generator, ME 280a HW 5
 
-Nt = 1;         % number of layers
+Nt = 2;         % number of layers
 No = 1;         % number of elements in the theta direction
 Nc = 8;         % number of elements in circumferential direction
 num_nodes_per_elem = 4;     % linear elements
 
 R = 1;          % radius of each arch
 r = 0.2;        % radius of inner hole
+t = 0.1;        % thickness of the tube wall
+
+layer_thickness = t / (Nt);           % thickness of each ring
+angle = (2*pi) / Nc;                % angle in horizontal plane
+
 
 % in a particular horizontal (relative to tube axis) plane:
 
@@ -14,17 +19,37 @@ r = 0.2;        % radius of inner hole
 % until reaching the outer surface
 
 % each row represents one coordinate of a global node
-coordinates = zeros(Nt * Nc * No * num_nodes_per_elem, 3);
+coordinates = zeros(Nt * Nc, 3);
 
-angle = (2*pi) / Nc;
+
 theta = 0;
-for i = 1:Nc % create a layer
+dt = 0;          
+
+k = 1; % index for coordinate row
+for j = 1:(Nt + 1) % create all layers of rings
     
-    % x-coordinate
-    coordinates(i, 1) = r * cos(theta);
-    theta = theta + angle;
+    for i = 1:Nc % create a single ring
+
+        % x-coordinate
+        coordinates(k, 1) = (r + dt) * cos(theta);
+
+        % y-coordinate
+        coordinates(k, 2) = (r + dt) * sin(theta);
+
+        % z-coordinate
+        coordinates(k,3) = 0;
+        
+        k = k + 1;
+
+        theta = theta + angle;
+    end
+    
+    dt = dt + layer_thickness; % reset radius
+    theta = 0; % reset angle
 end
 
+x = coordinates(:,1);
+y = coordinates(:,2);
+z = coordinates(:,3);
 
-
-
+scatter3(x,y,z)
